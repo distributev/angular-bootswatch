61dfad8c598f53aaa80b335271fb19177046f869
Jiahong <wengjiahong@gmail.com>
2016-05-17 09:03:26 -0500

    Add files via upload

--- test/spec.js
+++ test/spec.js
@@ -1,13 +1,13 @@
-describe('Protractor Demo App', function() {
+describe('Protractor bootswatch App', function() {
   it('login and using the saved bootstrap theme', function() {
     browser.get('http://localhost/bootswatch/app/index.html#/login');
 
+    //login
     element(by.model('user.username')).sendKeys("user1");
     element(by.model('user.password')).sendKeys("123");
-    
     element(by.id('submit')).click();
 
-
+    // select the theme
     var eleIds = ["default","cerulean","cosmo","cyborg","darkly","flatly","journal","lumen","paper","readable","sandstone","simplex","slate","spacelab","superhero","united","yeti"];
     var index = Math.floor(Math.random() * 10) + 1;
 
@@ -17,15 +17,18 @@ describe('Protractor Demo App', function() {
 	browser.wait(EC.elementToBeClickable($('#'+eleIds[index])), 1000);
     element(by.id(eleIds[index])).click();
 
+    //check the current theme
     element(by.id('bootstrap_theme')).getAttribute("href").then(function(value){
     	if(index!=0)
 			expect(value).toContain(eleIds[index]);
-		else
+		else //defult theme will use href="#", so the href attribute will be current url
 			expect(value).toContain("http://localhost/bootswatch/app/index.html#");
     });
 
+    //logout
     element(by.id('logout')).click();
 
+    //login again to see if theme is the one last time selected;
     element(by.model('user.username')).sendKeys("user1");
     element(by.model('user.password')).sendKeys("123");
     
@@ -38,8 +41,5 @@ describe('Protractor Demo App', function() {
 			expect(value).toContain("http://localhost/bootswatch/app/index.html#");
     });
     
- 
-
-
   });
 });
