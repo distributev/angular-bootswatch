910b56bff9d67b8090b1ca30946ac00c6f7dd4b5
mike <wengjiahong@gmail.com>
2016-05-20 02:04:43 +0000

    code clean

--- app/js/directive/fileManager/nodejs/config/express.js
+++ app/js/directive/fileManager/nodejs/config/express.js
@@ -2,13 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var timeout = require('connect-timeout');
 var cors = require('cors');
-var morgan = require('morgan');
 
 /**
  * Express configuration
  */
 module.exports = function(app) {
-	app.use(morgan('dev'));
 	app.use(bodyParser.json());
 	app.use(timeout('5s'));
 	app.use(cors());
